8ce503e7276ecb5e901ec2286ac158329a3d021b
Priyanka <priyanka.rel6@gmail.com>
2025-06-24 17:37:50 +0530

    added private and database subnet ids in output file

--- outputs.tf
+++ outputs.tf
@@ -5,3 +5,11 @@ output "vpc_id" {
 output "public_subnet_ids" {
   value = aws_subnet.public[*].id
 }
+
+output "private_subnet_ids" {
+  value = aws_subnet.private[*].id
+}
+
+output "database_subnet_ids" {
+  value = aws_subnet.database[*].id
+}
