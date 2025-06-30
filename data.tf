29ad09a128bbe9807cab73fb7e9c42213c2c32e4
Priyanka <priyanka.rel6@gmail.com>
2025-06-30 19:07:00 +0530

    publishing the outputs in readme file

--- data.tf
+++ data.tf
@@ -2,9 +2,9 @@ data "aws_availability_zones" "available" {
   state = "available"
 }
 
-# output "azs_info" {
-#   value = data.aws_availability_zones.available
-# }
+output "azs_info" {
+  value = data.aws_availability_zones.available
+}
 
 data "aws_vpc" "default" {
   default = true
